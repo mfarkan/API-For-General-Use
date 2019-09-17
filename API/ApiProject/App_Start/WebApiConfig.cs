3f267e2eca94758f7fccd4cde233f38b9882269a
Devil's Horn <muratfatiharkan@yandex.com.tr>
2019-09-17 11:46:42 +0300

    Add project files.

---
+++ API/ApiProject/App_Start/WebApiConfig.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Formatting;
+using System.Net.Http.Headers;
+using System.Web.Http;
+
+namespace ApiProject
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            config.Formatters.JsonFormatter.MediaTypeMappings.Add(
+                 new QueryStringMapping("type", "json", new MediaTypeHeaderValue("application/json")));
+
+            config.Formatters.XmlFormatter.MediaTypeMappings.Add(
+                new QueryStringMapping("type", "xml", new MediaTypeHeaderValue("application/xml")));
+
+
+            config.MapHttpAttributeRoutes();
+
+            config.EnableCors();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
---
+++ API/ApiProject/Startup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Web.Http;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(ApiProject.Startup))]
+
+namespace ApiProject
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            HttpConfiguration config = new HttpConfiguration();
+            WebApiConfig.Register(config);
+            SwaggerConfig.Register(config);
+            app.UseWebApi(config);
+            // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=316888
+        }
+    }
+}
