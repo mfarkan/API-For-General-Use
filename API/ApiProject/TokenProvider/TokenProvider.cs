bcd66f4e98891a85324a7a22af415b088814f430
Murat Fatih ARKAN <muratfatiharkan@yandex.com.tr>
2019-09-18 13:59:59 +0300

    adding comment on TokenProvider.

--- API/ApiProject/TokenProvider/TokenProvider.cs
+++ API/ApiProject/TokenProvider/TokenProvider.cs
@@ -15,6 +15,13 @@ namespace ApiProject.TokenProvider
         {
             context.Validated();
         }
+        /// <summary>
+        /// Called when a request to the Token endpoint arrives with a "grant_type" of "password".
+        /// This occurs when the user has provided name and password credentials directly into the client application's user interface,
+        /// and the client application is using those to acquire an "access_token" and optional "refresh_token"
+        /// </summary>
+        /// <param name="context"></param>
+        /// <returns></returns>
         public override async Task GrantResourceOwnerCredentials(OAuthGrantResourceOwnerCredentialsContext context)
         {
             context.OwinContext.Response.Headers.Add("Access-Control-Allow-Origin", new[] { "*" });
@@ -43,10 +50,17 @@ namespace ApiProject.TokenProvider
             }
             else
             {
-                context.SetError("invalid_grant", "Yetkisiz Erişim!");
+                context.SetError("invalid_grant", "Unauthorized");
                 context.Rejected();
             }
         }
+        /// <summary>
+        /// Called at the final stage of a successful Token endpoint request. 
+        /// An application may implement this call in order to do any final modification of the claims being used to issue access or refresh tokens. 
+        /// This call may also be used in order to add additional response parameters to the Token endpoint's json response body.
+        /// </summary>
+        /// <param name="context"></param>
+        /// <returns></returns>
         public override Task TokenEndpoint(OAuthTokenEndpointContext context)
         {
             foreach (KeyValuePair<string, string> property in context.Properties.Dictionary)
